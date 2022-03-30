1f40c0a4e5a3cfb9e868d64d7983b095c9a8e98f
valeckom <valeckom@gmail.com>
2022-03-30 12:49:50 -0700

    mirror page init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,21 @@
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
 
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./home/home.module').then(
+      m => m.HomePageModule),
   },
   {
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    redirectTo: 'mirror',
+    pathMatch: 'full',
+  },
+  {
+    path: 'mirror',
+    loadChildren: () => import('./mirror/mirror.module').then(
+      m => m.MirrorPageModule),
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,29 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {RouteReuseStrategy} from '@angular/router';
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import {IonicModule, IonicRouteStrategy} from '@ionic/angular';
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+import {AppComponent} from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {CameraPreview} from '@awesome-cordova-plugins/camera-preview/ngx';
+import {
+  AndroidFullScreen,
+} from '@awesome-cordova-plugins/android-full-screen/ngx';
+import {
+  ScreenOrientation,
+} from '@awesome-cordova-plugins/screen-orientation/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    {provide: RouteReuseStrategy, useClass: IonicRouteStrategy},
+    CameraPreview,
+    AndroidFullScreen,
+    ScreenOrientation],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+}
